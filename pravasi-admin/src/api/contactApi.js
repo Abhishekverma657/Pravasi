68554b873942d175498c3f898c0a0b44e6681d03
garvitasingh <garvitasingh936@gmal.com>
2025-12-15 18:17:32 +0530

    unread contact fixed

--- pravasi-admin/src/api/contactApi.js
+++ pravasi-admin/src/api/contactApi.js
@@ -19,8 +19,9 @@ export const deleteContact = async (id) => {
 };
 
 export const toggleReadStatus = async (id, read) => {
+  console.log(read);
   try {
-    const res = await axiosClient.patch(`/admin/contact-message/${id}/read`, { read });
+    const res = await axiosClient.patch(`/admin/contact-message/${id}/${read?'read':'unread'}`,);
     return res.data;
   } catch (err) {
     throw new Error(err.response?.data?.message || err.message || "Failed to update status");
