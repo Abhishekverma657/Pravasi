3e54bb99c73b14a14a46f0d5c116d3dd397905c1
garvitasingh <garvitasingh936@gmal.com>
2026-01-03 12:55:49 +0530

    url update

--- pravasi-admin/src/utils/constants.js
+++ pravasi-admin/src/utils/constants.js
@@ -3,6 +3,6 @@
 // export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || "http://pravasi-backend-env.eba-myxwcnwj.ap-south-1.elasticbeanstalk.com";
 
 export const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://pravasi-backend-env.eba-myxwcnwj.ap-south-1.elasticbeanstalk.com/api";
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || "http://pravasi-backend-env.eba-myxwcnwj.ap-south-1.elasticbeanstalk.com";
+  import.meta.env.VITE_API_BASE_URL || "https://rajasthanpravasi.in/api";
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || "https://rajasthanpravasi.in";
 
